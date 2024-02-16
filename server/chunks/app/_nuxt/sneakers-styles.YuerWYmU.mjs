980621337d22a25ed3f0ae1b6334bb40c0a92476
Sirojiddin06 <sirojiddinmirahmad@gmail.com>
2024-02-16 18:13:48 +0200

    deploy

--- server/chunks/app/_nuxt/sneakers-styles.YuerWYmU.mjs
+++
@@ -1,6 +0,0 @@
-const sneakers_vue_vue_type_style_index_0_lang = "body{background-color:#fff}";
-
-const sneakersStyles_YuerWYmU = [sneakers_vue_vue_type_style_index_0_lang, sneakers_vue_vue_type_style_index_0_lang];
-
-export { sneakersStyles_YuerWYmU as default };
-//# sourceMappingURL=sneakers-styles.YuerWYmU.mjs.map
